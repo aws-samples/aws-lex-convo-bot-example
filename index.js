22447730495b0a5ce2d46476f30315de6519158f
Pubali Sen <pubali.sen@sjsu.edu>
2017-06-19 11:47:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var omdb = require('omdb');
+const tmdb = require('tmdbv3').init('35440259b50e646a6485055c83367ccd');
      
     // Close dialog with the customer, reporting fulfillmentState of Failed or Fulfilled ("Thanks, your pizza will arrive in 20 minutes")
     function close(sessionAttributes, fulfillmentState, message) {
@@ -19,65 +19,36 @@ var omdb = require('omdb');
         console.log('request received for userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.intentName}');
         const sessionAttributes = intentRequest.sessionAttributes;
         const slots = intentRequest.currentIntent.slots;
-        const moviename = slots.name;
-        const whatInfo = slots.summary;
+        var moviename = slots.name;
+        var whatInfo = slots.summary;
         console.log(`request received for Slots=${moviename}, ${whatInfo}`);
         
-        
-        omdb.get({ title: moviename  }, true, function(err, movie) {
-    if(err) {
-        return console.error(err);
-    }
-
-    if(!movie) {
-        return console.log('Movie not found!');
-    }
-    var movietitle = movie.title;
-    var date = movie.year;
-    var actors = movie.actors;
-    var rating = movie.imdb.rating;
-    var plot = movie.plot;
-    var votes = movie.imdb.votes;
-    var director =movie.director;
-
-       if (whatInfo === 'Rating' || whatInfo === 'rating'){
+        tmdb.search.movie( `${moviename}`, function(err,res) {
+    if (err)
+        console.log(err);
+    else
+        tmdb.movie.info(res.results[0].id, function(err,res){
+          var resPlot = res.overview;
+          var resDate = res.release_date;
+          console.log(res);
+if  (whatInfo === 'Year' || whatInfo === 'Release Date' || whatInfo === 'release date'|| whatInfo === 'date'|| whatInfo === 'date'|| whatInfo === 'Release Year' || whatInfo === 'release year' || whatInfo === 'year'){
         callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `Rating of ${moviename} is ${rating}`}));
-        
-    }
-      else if (whatInfo === 'Actors' || whatInfo === 'actors' || whatInfo === 'actor' || whatInfo === 'Actor'){
-        callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `Actors: ${actors}`}));
+        {'contentType': 'PlainText', 'content': ` ${moviename} released in: ${resDate}`}));
       }
 
-      else if (whatInfo === 'votes' || whatInfo === 'vote' || whatInfo === 'Votes' || whatInfo === 'Vote'){
-        callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `Votes for ${moviename}: ${votes}`}));
-      }
-      
+
       else if (whatInfo === 'Plot' || whatInfo === 'Story' || whatInfo === 'plot' || whatInfo === 'story'){
         callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `Plot of ${moviename} is: ${plot}`}));
-      }
-      
-      else if (whatInfo === 'Year' || whatInfo === 'Release Date' || whatInfo === 'release date'|| whatInfo === 'Release Year' || whatInfo === 'release year' || whatInfo === 'year'){
-        callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': ` ${moviename} released in: ${date}`}));
-      }
-      
-      else if (whatInfo === 'Director' || whatInfo === 'director'){
-        callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `Director of ${moviename} is/are: ${director}`}));
+        {'contentType': 'PlainText', 'content': `Plot of ${moviename} is: ${resPlot}`}));
       }
 
-
-       else
+      else
         callback(close(sessionAttributes, 'Fulfilled',
-        {'contentType': 'PlainText', 'content': `MovieName: ${moviename}, Year: ${date}, Actors: ${actors}, Rating: ${rating}, Plot: ${plot} Votes: ${votes} Director: ${director}`}));
-        
-    }
-    )}
-     
+        {'contentType': 'PlainText', 'content': `MovieName: ${moviename}, Year: ${resDate}, APlot: ${resPlot}`}));
+});
+});
+}
+   
     // --------------- Main handler -----------------------
      
     // Route the incoming request based on intent.
@@ -92,3 +63,4 @@ var omdb = require('omdb');
             callback(err);
         }
     };
+
